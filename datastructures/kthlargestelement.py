71913952283a700dfbcfc3894d0a6d9efa008eb6
Victor H Damian Chicon <victorhdamianchicon@Victors-MacBook-Pro.local>
2025-04-16 19:03:46 -0400

    added silest sol

--- datastructures/kthlargestelement.py
+++ datastructures/kthlargestelement.py
@@ -83,6 +83,9 @@ def kthLargestElement(nums: list[int], k: int) -> int:
 nums = [3,2,1,5,6,4]
 k = 2
 print(kthLargestElement(nums, k))
+print(sorted(nums)[-k])
 nums = [3,2,3,1,2,4,5,5,6]
 k = 4
 print(kthLargestElement(nums, k))
+print(sorted(nums)[-k])
+
